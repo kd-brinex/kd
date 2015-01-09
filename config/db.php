b9db98638c28d5968dfc1ebe20ca8f6cf2984652
Khusainov Marat <hmf@yandex.ru>
2015-01-09 17:15:52 +0300

    Добавил модуль Товары. Отображает список категории для магазина 999

---
+++ config/db.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'class'=>'yii\db\Connection',
+    'dsn'=>'mysql:host=localhost;dbname=brinex1',
+    'username'=>'brinex',
+    'password'=>'G90BqaKJ',
+    'charset'=>'utf8',
+];
