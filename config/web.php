284f9858cbcfac9d267b4b2a40684b2659cb3dd2
Khusainov <hmf@yandex.ru>
2015-01-18 19:59:05 +0400

    1

--- config/web.php
+++ config/web.php
@@ -1,18 +1,21 @@
 <?php
 $params = require(__DIR__ . '/params.php');
-$db_connect=require(__DIR__ . '/db.php');
+$db_connect = require(__DIR__ . '/db.php');
 $config = [
     'id' => 'basic',
     'basePath' => dirname(__DIR__),
     'language' => 'ru-RU',
     'bootstrap' => ['log'],
-    'name'=>'Колеса-даром',
+    'name' => 'Колеса-даром',
     'components' => [
+        'city'=>[
+          'class'=>'app\modules\city\City'
+        ],
         'ipgeobase' => [
             'class' => 'app\modules\city\IpGeoBase',
             'useLocalDB' => true,
         ],
-        'urlManager'=>[
+        'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
 //            'enableStrictParsing' => true,
@@ -20,21 +23,23 @@ $config = [
             'rules' => [
                 '' => 'site/index',
                 'login' => 'user/security/login',
-                'register'=> 'user/registration/register',
+                'register' => 'user/registration/register',
                 'about' => 'site/about',
                 'contact' => 'site/contact',
-                'profile'=>'user/settings/profile',
-                'ugb'=>'site/ugb',
+                'profile' => 'user/settings/profile',
+                'ugb' => 'site/ugb',
+                'city'=>'city/city/index',
 //                'gii' => 'yii/gii',
 //                '<controller:\w+>/<id:\d+>' => '<controller>/view',
 //                '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
 //                '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
 //                'module/<module:\w+>/<controller:\w+>/<action:\w+>' => '<module>/<controller>/<action>',
 //                'module/<module:\w+>/<controller:\w+>/<action:\w+>/<id:\w+>' => '<module>/<controller>/<action>/<id>',
-            'tovar/<id:\w+>'=>'tovar/tovar/view',
-            'tovars/<tip_id:\w+>'=>'tovar/tovar/category'
+                'tovar/<id:\w+>' => 'tovar/tovar/view',
+                'tovars/<tip_id:\w+>' => 'tovar/tovar/category',
+
             ],
-           ],
+        ],
 
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
@@ -126,8 +131,8 @@ $config = [
     ],
     'params' => $params,
     'modules' => [
-        'tovar'=>[
-            'class'=>'app\modules\tovar\Tovar',
+        'tovar' => [
+            'class' => 'app\modules\tovar\Tovar',
         ],
         'user' => [
             'class' => 'dektrium\user\Module',
---
+++ modules/city/City.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace app\modules\city;
+
+class City extends \yii\base\Module
+{
+    public $controllerNamespace = 'app\modules\city\controllers';
+
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
