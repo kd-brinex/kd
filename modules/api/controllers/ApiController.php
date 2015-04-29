7513a9a0eb8c6db51e28c9c9018c7477e449f8aa
Khusainov Marat <hmf@yandex.ru>
2015-04-29 17:23:59 +0300

    layout false api

--- modules/api/controllers/ApiController.php
+++ modules/api/controllers/ApiController.php
@@ -7,6 +7,7 @@ class ApiController extends Controller
 {
     public function actionFinddetails()
     {
+        $this->layout=false;
     $params = \Yii::$app->request->queryParams;
     $details=Api::findDetails($params);
 //    var_dump($details);die;
