f6998e8915462635e3f96797f93d0fe011c24ac6
Khusainov Marat <hmf@yandex.ru>
2015-04-30 10:19:50 +0300

    Убрал пробелы из xml

--- modules/api/models/Api.php
+++ modules/api/models/Api.php
@@ -15,28 +15,27 @@ class Api extends Model
         $xml='<?xml version="1.0" encoding="utf-8"?>
 <ArrayOfDetailInfo>';
         foreach($details as $d){
-            $xml.='
-        <DetailInfo>
-        <detailnumber>'.$d['code'].'</detailnumber>
-        <detailname>'.$d['name'].'</detailname>
-        <maker>
-            <id>-</id>
-            <name>'.$d['manufacture'].'</name>
-        </maker>
-        <quantity>'.$d['quantity'].'</quantity>
-        <lotquantity>'.$d['lotquantity'].'</lotquantity>
-        <price>'.$d['price'].'</price>
-        <pricedestination>'.$d['pricedestination'].'</pricedestination>
-        <days>'.$d['srokmin'].'</days>
-        <dayswarranty>'.$d['srokmax'].'</dayswarranty>
-        <regionname>'.$d['sklad'].'</regionname>
-        <estimation>'.$d['estimation'].'</estimation>
-        <orderrefernce>'.$d['reference'].'</orderrefernce>
-        <pricedate>'.$d['pricedate'].'</pricedate>
-        <groupid>'.$d['groupid'].'</groupid>
-        <provider>'.$d['provider'].'</provider>
-        </DetailInfo>';
-        }
+            $xml.='<DetailInfo>
+<detailnumber>'.$d['code'].'</detailnumber>
+<detailname>'.$d['name'].'</detailname>
+<maker>
+<id>-</id>
+<name>'.$d['manufacture'].'</name>
+</maker>
+<quantity>'.$d['quantity'].'</quantity>
+<lotquantity>'.$d['lotquantity'].'</lotquantity>
+<price>'.$d['price'].'</price>
+<pricedestination>'.$d['pricedestination'].'</pricedestination>
+<days>'.$d['srokmin'].'</days>
+<dayswarranty>'.$d['srokmax'].'</dayswarranty>
+<regionname>'.$d['sklad'].'</regionname>
+<estimation>'.$d['estimation'].'</estimation>
+<orderrefernce>'.$d['reference'].'</orderrefernce>
+<pricedate>'.$d['pricedate'].'</pricedate>
+<groupid>'.$d['groupid'].'</groupid>
+<provider>'.$d['provider'].'</provider>
+</DetailInfo>';
+}
         $xml.='
 </ArrayOfDetailInfo>';
 return $xml;
