69d94a125d7c42d1fb51fef008cf73b022be6b1c
eugene <compuniti@mail.ru>
2015-09-09 11:32:53 +0300

    Задача - Корзина.Вид

--- modules/autoparts/views/orders/orders.php
+++ modules/autoparts/views/orders/orders.php
@@ -98,34 +98,34 @@ $this->params['breadcrumbs'][] = $this->title;
                 'label' => 'Дата',
                 'value' => 'datetime'
             ],
-            [
-                'class' => 'kartik\grid\EditableColumn',
-                'label' => 'Платеж',
-                'attribute' => 'pay_datetime',
-                'editableOptions'=> function ($model, $key, $index) {
-                    return [
-                        'header'=>'дату платежа',
-                        'size'=>'md',
-                        'inputType' => \kartik\editable\Editable::INPUT_WIDGET,
-                        'widgetClass' => '\kartik\widgets\DateTimePicker',
-                        'options' => [
-                           'value' => $model['pay_datetime'],
-                           'pluginOptions' => [
-                               'autoclose'=>true,
-                               'format' => 'dd.mm.yyyy hh:mm'
-                           ]
-                        ],
-                        'formOptions' => [
-                            'action' => 'update'
-                        ],
-
-
-                     ];
-                },
-                'value' => 'normalizeDate'
-
-
-            ],
+//            [
+//                'class' => 'kartik\grid\EditableColumn',
+//                'label' => 'Платеж',
+//                'attribute' => 'pay_datetime',
+//                'editableOptions'=> function ($model, $key, $index) {
+//                    return [
+//                        'header'=>'дату платежа',
+//                        'size'=>'md',
+//                        'inputType' => \kartik\editable\Editable::INPUT_WIDGET,
+//                        'widgetClass' => '\kartik\widgets\DateTimePicker',
+//                        'options' => [
+//                           'value' => /*$model['pay_datetime']*/'',
+//                           'pluginOptions' => [
+//                               'autoclose'=>true,
+//                               'format' => 'dd.mm.yyyy hh:mm'
+//                           ]
+//                        ],
+//                        'formOptions' => [
+//                            'action' => 'update'
+//                        ],
+//
+//
+//                     ];
+//                },
+//                'value' => 'normalizeDate'
+//
+//
+//            ],
             [
                 'class' => 'kartik\grid\ExpandRowColumn',
                 'expandAllTitle' => 'Место доставки',
