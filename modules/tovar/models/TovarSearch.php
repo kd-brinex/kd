992576fc57cc70ca8b7b4be18cc4c3e9dbf5b3ca
Khusainov Marat <hmf@yandex.ru>
2015-11-17 16:00:49 +0300

    Не показывать незаполненные характеристики товара

--- modules/tovar/models/TovarSearch.php
+++ modules/tovar/models/TovarSearch.php
@@ -80,7 +80,7 @@ class TovarSearch extends Tovar
     public function find_tovar_param($params)
     {
         $p['id'] = $params['id'];
-        $query = TovarParam::find()->andwhere('(id=:id) and (title<>\'\') and not (value_char is null)', $p);
+        $query = TovarParam::find()->andwhere('(id=:id) and (title<>\'\') and (value_char <>\'\')', $p);
         $dataProvider = new ActiveDataProvider(['query' => $query]);
         return $dataProvider;
     }
